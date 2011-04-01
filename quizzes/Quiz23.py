651028bdc93986289b3d48ad805a781e206e518e
Glenn Downing <downing@cs.utexas.edu>
2011-04-01 15:09:34 -0500

    another commit

---
+++ quizzes/Quiz23.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #23 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. Features of the financial contract language MLFi, for example, were
+   inspired by what other language that, while not currently pervasive,
+   is often examined in programming languages courses.
+   [Why Undergraduates Should Learn the Principles of Programming
+    Languages]
+   (1 pt)
+
+Haskell
+"""
+
+""" ----------------------------------------------------------------------
+2. What is the benefit that relational databases are starting to NOT be
+   able to deliver on?
+   [Is the Relational Database Doomed?]
+   (1 pt)
+
+scalability
+"""
+
+""" ----------------------------------------------------------------------
+3. What is the output of the following program?
+   (2 pts)
+
+baC
+"""
+
+s = "abCbA"
+t = 2 * s
+print t[-7:8:2]
